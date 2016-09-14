731e5cfec1903f20701e6ce5ff378ecd0e74f983
developer388 <nickhil388@yandex.com>
2016-09-14 17:06:38 +0530

    port changed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.use(express.static(__dirname+'/',{redirect:false}));
 app.listen(port,function(){
 	console.log("Server Started, Port : "+port);
